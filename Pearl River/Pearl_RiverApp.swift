1474328982591f67e0ddaf5268e5a425097c92bc
Radcabna <radcabna@gmail.com>
2025-04-09 14:36:29 +0300

    Initial Commit

---
+++ Pearl River/Pearl_RiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pearl_RiverApp.swift
+//  Pearl River
+//
+//  Created by Алкександр Степанов on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pearl_RiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
